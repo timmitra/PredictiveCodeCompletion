9f1364d0b535ecc045fededf41dce8cd7e5d1750
Tim Mitra <tim@it-guy.com>
2024-10-09 02:28:06 -0400

    Used Xcode 16 predictive code completion from Rebeloper video

--- PredictiveCodeCompletion/ContentView.swift
+++ PredictiveCodeCompletion/ContentView.swift
@@ -12,17 +12,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var profiles: [Profile] = [
+        .init(name: "Tim Mitra"),
+        .init(name:"John Doe"),
+        .init(name:"Jane Smith"),
+        .init(name: "Jane Doe")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(profiles) { profile in
+                Text(profile.name)
+                    .font(.headline)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+// a profile struct that is Codable and Identifiable and has a name variable property
+struct Profile: Codable, Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
 }
